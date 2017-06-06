b6fa91ee8f4cbcf06e3c103f80faa03ef2be306e
nicobuton37 <nicobuton370@gmail.com>
2017-06-06 10:55:16 +0200

    entities and categories

---
+++ src/main/java/com/nicolas/eBoutique/entities/Categorie.java
@@ -0,0 +1,63 @@
+package com.nicolas.eBoutique.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public class Categorie implements Serializable {
+	private Long idCategorie;
+	private String nomCategorie;
+	private String description;
+	private byte[] photo;
+	private String nomPhoto;
+	private Collection<Produit> produits;
+	
+	
+	public Categorie(String nomCategorie, String description, byte[] photo, String nomPhoto) {
+		super();
+		this.nomCategorie = nomCategorie;
+		this.description = description;
+		this.photo = photo;
+		this.nomPhoto = nomPhoto;
+	}
+	
+	public Categorie() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Long getIdCategorie() {
+		return idCategorie;
+	}
+	public void setIdCategorie(Long idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+	public String getNomCategorie() {
+		return nomCategorie;
+	}
+	public void setNomCategorie(String nomCategorie) {
+		this.nomCategorie = nomCategorie;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public byte[] getPhoto() {
+		return photo;
+	}
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+	public String getNomPhoto() {
+		return nomPhoto;
+	}
+	public void setNomPhoto(String nomPhoto) {
+		this.nomPhoto = nomPhoto;
+	}
+	public Collection<Produit> getProduits() {
+		return produits;
+	}
+	public void setProduits(Collection<Produit> produits) {
+		this.produits = produits;
+	}
+}
